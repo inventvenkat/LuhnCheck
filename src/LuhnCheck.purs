ca101881463db1e02c8234c5df0ebd19d9bc1206
venktesh_juspay <venkatesh.devendran@juspay.in>
2018-08-28 12:40:54 +0530

    handled extra cases

--- src/LuhnCheck.purs
+++ src/LuhnCheck.purs
@@ -2,8 +2,8 @@ module LuhnCheck where
 
 import Prelude
 
-import Data.Array (head, reverse, tail)
-import Data.Int (odd, round)
+import Data.Array (head, length, reverse, tail)
+import Data.Int (odd)
 import Data.Maybe (fromMaybe)
 import Data.Number (fromString)
 import Data.String (Pattern(..), Replacement(..), replaceAll, split)
@@ -12,11 +12,13 @@ import Math (remainder)
 luhnCheck :: String -> Boolean
 luhnCheck cardNumber = 
   let ref = reverse $ split (Pattern "") (replaceAll (Pattern " ") (Replacement "") cardNumber)
-      sum = luhnSum 0.0 ref in
-  (remainder sum 10.0) == 0.0
+      sum = luhnSum 0 ref in
+  (remainder sum 10.0) == 0.0 && (length ref /= 0) && sum /= 0.0
   
-luhnSum :: Number -> Array String -> Number
+luhnSum :: Int -> Array String -> Number
 luhnSum _ [] = 0.0
-luhnSum i xs = if odd $ round i
-  then (remainder (2.0 * (fromMaybe 0.0 $ fromString $ fromMaybe "0" $ head xs)) 9.0) + (luhnSum (i+1.0) $ fromMaybe [] $ tail xs)
-  else (fromMaybe 0.0 $ fromString $ fromMaybe "0" $ head xs) + (luhnSum (i+1.0) $ fromMaybe [] $ tail xs)
+luhnSum i xs = 
+  let firstNumber = fromMaybe 0.0 $ fromString $ fromMaybe "0" $ head xs in
+  if odd i
+    then (remainder (2.0 * firstNumber) 9.0) + (luhnSum (i+1) $ fromMaybe [] $ tail xs)
+    else firstNumber + (luhnSum (i+1) $ fromMaybe [] $ tail xs)
