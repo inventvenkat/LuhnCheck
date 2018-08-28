861b8c278b8f921cdc804a6be08f983c6653a8ca
venktesh_juspay <venkatesh.devendran@juspay.in>
2018-08-28 12:53:23 +0530

    handled for cardnumbers with and without spaces

--- src/LuhnCheck.purs
+++ src/LuhnCheck.purs
@@ -9,12 +9,17 @@ import Data.Number (fromString)
 import Data.String (Pattern(..), Replacement(..), replaceAll, split)
 import Math (remainder)
 
+-- card Luhn check
 luhnCheck :: String -> Boolean
 luhnCheck cardNumber = 
-  let ref = reverse $ split (Pattern "") (replaceAll (Pattern " ") (Replacement "") cardNumber)
+  let ref = reverse $ flip split cardNumber $ Pattern ""
       sum = luhnSum 0 ref in
   (remainder sum 10.0) == 0.0 && (length ref /= 0) && sum /= 0.0
   
+-- Ignores Spaces
+luhnCheck' :: String -> Boolean
+luhnCheck' cardNumber = luhnCheck $ replaceAll (Pattern " ") (Replacement "") cardNumber
+
 luhnSum :: Int -> Array String -> Number
 luhnSum _ [] = 0.0
 luhnSum i xs = 
