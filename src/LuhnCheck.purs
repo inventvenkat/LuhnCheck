ec6fa951ec6e50312f2742f047f89fb225e9e8ff
venktesh_juspay <venkatesh.devendran@juspay.in>
2018-08-29 12:21:50 +0530

    luhn check will result false for card number containing other than numbers and spaves

--- src/LuhnCheck.purs
+++ src/LuhnCheck.purs
@@ -1,13 +1,14 @@
-module LuhnCheck(luhnCheck) where
+module LuhnCheck(luhnCheck,checkAlpha) where
 
 import Prelude
 
 import Data.Array (head, length, reverse, tail)
-import Data.Int (odd)
+import Data.Int (odd,fromString)
 import Data.Maybe (fromMaybe)
-import Data.Number (fromString)
 import Data.String (Pattern(..), Replacement(..), replaceAll, split)
-import Math (remainder)
+import Data.Either (Either(Right,Left), either)
+import Data.String.Regex (regex, test)
+import Data.String.Regex.Flags (noFlags)
 
 -- |        Luhn Algo 
 -- |
@@ -26,20 +27,25 @@ import Math (remainder)
 -- | check it is divisible by 10
 
 -- Card Luhn check
-luhnCheck :: String -> Boolean
-luhnCheck cardNumber = 
-  let ref = reverse $ flip split (replaceSpace cardNumber) $ Pattern ""
-      sum = luhnSum 0 ref in
-  (remainder sum 10.0) == 0.0 && (length ref /= 0) && sum /= 0.0
-  
--- Ignores Spaces
-replaceSpace :: String -> String
-replaceSpace = replaceAll (Pattern " ") (Replacement "")
 
-luhnSum :: Int -> Array String -> Number
-luhnSum _ [] = 0.0
+checkAlpha :: String -> Either String String
+checkAlpha cardNumber = do
+  regexVal <- regex "^[0-9\\s]*$" noFlags
+  if test regexVal cardNumber
+    then (Right cardNumber)
+    else (Left "Error")
+
+luhnCheck :: String -> Boolean
+luhnCheck = either (const false) (luhnCheck_ <<< reverse <<< split (Pattern "") <<< replaceAll (Pattern " ") (Replacement "")) <<< checkAlpha
+  where
+    luhnCheck_ ref =
+      let sum = luhnSum 0 ref
+      in (mod sum 10) == 0 && (length ref /= 0) && sum /= 0
+
+luhnSum :: Int -> Array String -> Int
+luhnSum _ [] = 0
 luhnSum i xs = 
-  let firstNumber = fromMaybe 0.0 $ fromString $ fromMaybe "0" $ head xs in
+  let firstNumber = fromMaybe 0 $ fromString =<< head xs in
   if odd i
-    then (remainder (2.0 * firstNumber) 9.0) + (luhnSum (i+1) $ fromMaybe [] $ tail xs)
-    else firstNumber + (luhnSum (i+1) $ fromMaybe [] $ tail xs)
+    then (mod (2 * firstNumber) 9) + (fromMaybe 0 (luhnSum (i+1) <$> tail xs))
+    else firstNumber + (fromMaybe 0 (luhnSum (i+1) <$> tail xs))
